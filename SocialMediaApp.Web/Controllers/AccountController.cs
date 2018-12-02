d50d341df2de336662c5f5779e5b960397f03240
Andrew Snook <acsnook@dmacc.edu>
2018-12-02 13:30:07 -0600

    Fixed "Log Off" link

--- SocialMediaApp.Web/Controllers/AccountController.cs
+++ SocialMediaApp.Web/Controllers/AccountController.cs
@@ -395,7 +395,7 @@ namespace SocialMediaApp.Web.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
