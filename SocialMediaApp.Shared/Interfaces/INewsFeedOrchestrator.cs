374d89df0011a8dc68232ade1e11ef721d42a880
Andrew Snook <acsnook@dmacc.edu>
2018-12-02 21:12:35 -0600

    --Added new interface "INewsFeedOrchestrator" to the "Shared" project.

---
+++ SocialMediaApp.Shared/Interfaces/INewsFeedOrchestrator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Shared.Interfaces
+{
+    interface INewsFeedOrchestrator
+    {
+        Task<List<NewsPostViewModel>> GetAllPosts();
+        Task<int> AddNewPost(NewsPostViewModel post);
+    }
+}
