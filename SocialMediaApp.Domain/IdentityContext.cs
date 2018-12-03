dc2654f4446ab68f8969cdef55ee8c5c8cee96e0
Andrew Snook <acsnook@dmacc.edu>
2018-12-03 00:37:52 -0600

    Fixed last commit.

--- SocialMediaApp.Domain/IdentityContext.cs
+++ SocialMediaApp.Domain/IdentityContext.cs
@@ -1,7 +1,14 @@
-﻿namespace SocialMediaApp.Domain
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SocialMediaApp.Domain.Entities;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SocialMediaApp.Domain
 {
     //=====================================================================================================================================================
-    /*  The old "socialContext" class is now at the bottom of this file. It has replaced "ApplicationDbContext".
+    /*  The old "socialContext" class is now at the bottom of this file. It has replaced "ApplicationDbContext". */
     //=====================================================================================================================================================
 
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
