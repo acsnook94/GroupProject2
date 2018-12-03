f7591d735cd738690b530755c77a24aacab5b768
Andrew Snook <acsnook@dmacc.edu>
2018-12-02 21:15:37 -0600

    --Added new "NewsPostViewModel" to the "Shared/ViewModels" folder.

---
+++ SocialMediaApp.Shared/ViewModels/NewsPostViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialMediaApp.Shared.ViewModels
+{
+    class NewsPostViewModel
+    {
+        public string UserName {get; set;}
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string PostTextBody {get; set;}
+    }
+}
